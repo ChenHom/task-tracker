8bdba79adb9efc0389b3e9fd2c733157bd07d054
bot109 <bot109@bot.com>
2026-08-11 23:10:43 +0800

    chore: remove generated sim artifacts

--- make_payload.py
+++
@@ -1,7 +0,0 @@
-import json
-
-with open('comment_body_b399.txt', 'r', encoding='utf-8') as f:
-    content = f.read()
-
-with open('comment_payload_b399.json', 'w', encoding='utf-8') as f:
-    json.dump({"content": content}, f, ensure_ascii=False)
